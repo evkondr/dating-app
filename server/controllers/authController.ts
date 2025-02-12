25a981900c73c61e113215607582a58c22e29326
evkondr <kondratiev.evg@gmail.com>
2025-02-12 21:36:30 +0300

    feat: add empty routes auth, match, message, user; add emty authController

---
+++ server/controllers/authController.ts
@@ -0,0 +1,7 @@
+import { Request, Response } from "express";
+
+export class AuthController {
+  static async signup(req:Request, res:Response){}
+  static async login(req:Request, res:Response){}
+  static async logout(req:Request, res:Response){}
+}
--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,20 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
+//Routes
+import authRoute from './routes/authRoute';
+import userRoute from './routes/userRoute';
+import matchRoute from './routes/matchRoute';
+import messageRoute from './routes/messageRoute';
+
 dotenv.config();
 const app = express();
 const port = process.env.PORT  || 5000;
+
+//Middleware
+app.use('/api/auth', authRoute);
+app.use('/api/users', userRoute);
+app.use('/api/matches', matchRoute);
+app.use('/api/messages', messageRoute);
 
 app.listen(5000, () => console.log(`Server is running on port ${port}`));
