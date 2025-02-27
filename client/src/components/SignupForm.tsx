852fe3d40caea18385925c20e0a043186139cf96
evkondr <kondratiev.evg@gmail.com>
2025-02-27 22:54:13 +0300

    init: signupForm

--- client/src/components/SignupForm.tsx
+++ client/src/components/SignupForm.tsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react';
+import TextField from './fields/TextField';
 
 const SignupForm = () => {
-  return (
-    <div>SignupForm</div>
-  )
-}
+  const [name, setName] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [gender, setGender] = useState<string>('');
+  const [age, setAge] = useState<string>('');
+  const [genderPreference, setGenderPreference] = useState<string>('');
 
-export default SignupForm
+  const signUp = () => {
+    console.log('sign up ');
+  };
+  return (
+    <form className="space-y-6" onSubmit={(e) => {
+      e.preventDefault();
+      signUp();
+    }}>
+      <div>
+        <TextField
+          label="Email"
+          id="email"
+          name="name"
+          type="email"
+          autoComplete="email"
+          value={email}
+          required
+          onChange={(e:React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
+        />
+      </div>
+      <div>
+        <TextField
+          label="Password"
+          id="password"
+          name="password"
+          type="password"
+          value={password}
+          required
+          onChange={(e:React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
+        />
+      </div>
+    </form>
+  );
+};
+
+export default SignupForm;
