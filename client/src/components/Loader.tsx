b8fbbcde3bb70afd79abfc47fd32625d18e2b8f8
evkondr <kondratiev.evg@gmail.com>
2025-03-18 21:12:46 +0300

    feat: add card layout

--- client/src/components/Loader.tsx
+++ client/src/components/Loader.tsx
@@ -2,7 +2,19 @@ import React from 'react';
 
 const Loader = () => {
   return (
-    <div>loading...</div>
+    <div className='relative w-full max-w-sm h-[28rem]'>
+      <div className='card bg-white w-96 h-[28rem] rounded-lg overflow-hidden border border-gray-200 shadow-sm'>
+        <div className='px-4 pt-4 h-3/4'>
+          <div className='w-full h-full bg-gray-200 rounded-lg' />
+        </div>
+        <div className='card-body bg-gradient-to-b from-white to-pink-50 p-4'>
+          <div className='space-y-2'>
+            <div className='h-6 bg-gray-200 rounded w-3/4' />
+            <div className='h-4 bg-gray-200 rounded w-1/2' />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -2,6 +2,10 @@
 import React, { useEffect } from 'react';
 import Sidebar from '../components/Sidebar';
 import useMatchStore from '../store/useMatch';
+import Header from '../components/Header';
+
+import NoMoreProfiles from '../components/NoMoreProfiles';
+import Loader from '../components/Loader';
 
 const HomePage = () => {
   const { getProfiles, profiles, loadingProfiles } = useMatchStore();
@@ -16,6 +20,19 @@ const HomePage = () => {
     <div className="flex flex-col lg:flex-row min-h-screen bg-gradient-to-br from-pink-100 to-purple-100
 		 overflow-hidden">
       <Sidebar />
+      <div className='flex-grow flex flex-col overflow-hidden'>
+        <Header />
+        <main className='flex-grow flex flex-col gap-10 justify-center items-center p-4 relative overflow-hidden'>
+          {profiles.length > 0 && !loadingProfiles && (
+            <div>
+              users
+            </div>
+          )}
+          {profiles.length === 0 && !loadingProfiles && <NoMoreProfiles />}
+
+          {loadingProfiles && <Loader />}
+        </main>
+      </div>
     </div>
   );
 };
